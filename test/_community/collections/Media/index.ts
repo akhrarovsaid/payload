5be4d13e6d9cadcb154f06cb1f8969f6d84d8496
akhrarovsaid <akhrarovsaid@gmail.com>
2024-12-22 12:48:08 -0500

    chore: minimal repro

--- test/_community/collections/Media/index.ts
+++ test/_community/collections/Media/index.ts
@@ -8,7 +8,90 @@ export const MediaCollection: CollectionConfig = {
     create: () => true,
     read: () => true,
   },
-  fields: [],
+  fields: [
+    {
+      type: 'text',
+      name: 'alt',
+    },
+    {
+      type: 'text',
+      name: 'readonlyText',
+      defaultValue: 'hello world',
+      admin: {
+        readOnly: true,
+      },
+    },
+    {
+      type: 'array',
+      name: 'readonlyArray',
+      defaultValue: [{ arrayText: 'Some array member in readonly array' }],
+      admin: {
+        readOnly: true,
+      },
+      fields: [
+        {
+          type: 'text',
+          name: 'arrayText',
+        },
+      ],
+    },
+    {
+      type: 'array',
+      name: 'editableWithReadonlyMemberArray',
+      defaultValue: [{ arrayText: 'Some array member in readonly array' }],
+      fields: [
+        {
+          type: 'text',
+          name: 'arrayReadonlyText',
+          defaultValue: 'Cant touch this',
+          admin: {
+            readOnly: true,
+          },
+        },
+      ],
+    },
+    {
+      type: 'array',
+      name: 'editableArray',
+      admin: {
+        readOnly: false,
+      },
+      fields: [
+        {
+          type: 'text',
+          name: 'arrayEditableText',
+        },
+      ],
+    },
+    {
+      type: 'tabs',
+      tabs: [
+        {
+          name: 'namedTab',
+          fields: [
+            {
+              type: 'text',
+              name: 'namedTabText',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'tabs',
+      tabs: [
+        {
+          label: 'unnamedTab',
+          fields: [
+            {
+              type: 'text',
+              name: 'unnamedTabText',
+            },
+          ],
+        },
+      ],
+    },
+  ],
   upload: {
     crop: true,
     focalPoint: true,
